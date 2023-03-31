db896145888c714def25bf3b9e6bdd153fda14ea
venchangming <128441368+venchangming@users.noreply.github.com>
2023-03-31 10:29:32 +0800

    with ramdisk

--- gen-pdf2image-cmd.js
+++ gen-pdf2image-cmd.js
@@ -1,15 +1,22 @@
-import {nodefs,writeChanged,parseXMLAttribute,peelXML,
-    patchBuf,meta_cbeta,
-    readTextContent, readTextLines,entity2unicode } from 'ptk/nodebundle.cjs'; //ptk/pali
+/* 
+	imagemagick create huge temp file ,
+	set TMP , TEMP to softPrefect ramdisk for better performance and reduce SSD write*/
+	
+import {nodefs,meta_cbeta, writeChanged } from 'ptk/nodebundle.cjs'; //ptk/pali
 await nodefs;
 
-const volperbatch=10;
+const fileperbatch=10;  //number of file extract by imagemagick on each invoke
+const volperbatch=10;  //each bat file has 10 volumns
+const imageformat='png';
+const outdir='d:/taisho-png/';
+
+const opts='';//'-gaussian-blur 0.1 -resize 50% -quality 5'; //around 200KB per page
 
 let pvol='';
-const show=()=>{
+const gen_cmd=()=>{
     const out=[];
     for (let v in meta_cbeta.MaxPage) {
-        const batchcount=Math.floor( meta_cbeta.MaxPage[v]  / 100) +1;
+        const batchcount=Math.floor( meta_cbeta.MaxPage[v]  / fileperbatch ) +1;
         v=v.toString().padStart(2,'0')
         if (pvol && pvol!==v.slice(0,1)) {
             writeChanged( 'taisho-'+pvol+'x.cmd',out.join('\n'),true)
@@ -17,12 +24,12 @@ const show=()=>{
         }
 
         for (let b=0;b<batchcount;b++) {
-            const vol='T'+v.padStart(2,'0'), from=b*100;
-            let to=b*100+99;
+            const vol='T'+v.padStart(2,'0'), from=b*fileperbatch;
+            let to=b*fileperbatch+fileperbatch-1;
             if (to>=meta_cbeta.MaxPage[v]) to=meta_cbeta.MaxPage[v]-1;
-            const opts='-gaussian-blur 0.1 -resize 50% -quality 5';
             out.push(`@echo ${vol} ${b+1}/${batchcount}`);
-            out.push(`@magick convert  ${opts} ${vol}.pdf[${from}-${to}] ${vol}/%04d.jpg`) 
+            //.cmd escape with double %
+            out.push(`@magick convert ${opts} ${vol}.pdf[${from}-${to}] ${outdir}${vol}/%%04d.`+imageformat) 
         }
 
         pvol=v.slice(0,1);
@@ -30,4 +37,4 @@ const show=()=>{
     writeChanged('taisho-'+pvol+'x.cmd',out.join('\n'),true)
 }
 
-show()
+gen_cmd();
